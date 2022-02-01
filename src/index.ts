2eb031404b8849f2ca942b589e08facc00498c14
Yhomira Yupayccana Lopa <yhomira.yupayccana@sofka.com.co>
2022-02-01 10:02:44 -0500

    08 classes parte 2

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,3 @@
     ===== Código de TypeScript =====
     
 */
-
-class Heroe{
-    private alterEgo:string;
-    public edad:number;
-    static nombreReal:number;
-
-
-}
-
-const iroman =new Heroe();
-console.log(iroman)
